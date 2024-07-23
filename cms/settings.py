0f724c4f7ad265fc0b3d5c41ac936a4cbb45af62
vpeopleonatank <vpeopleonatank@gmail.com>
2024-07-23 14:29:26 +0700

    no transcode - no download - disable review

--- cms/settings.py
+++ cms/settings.py
@@ -71,7 +71,7 @@ EMAIL_PORT = 587
 ADMIN_EMAIL_LIST = ["info@mediacms.io"]
 
 
-MEDIA_IS_REVIEWED = False  # whether an admin needs to review a media file.
+MEDIA_IS_REVIEWED = True  # whether an admin needs to review a media file.
 # By default consider this is not needed.
 # If set to False, then each new media need be reviewed otherwise
 # it won't appear on public listings
@@ -490,7 +490,7 @@ if GLOBAL_LOGIN_REQUIRED:
     ]
 
 # if True, only show original, don't perform any action on videos
-DO_NOT_TRANSCODE_VIDEO = False
+DO_NOT_TRANSCODE_VIDEO = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
